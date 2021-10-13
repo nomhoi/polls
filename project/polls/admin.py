533b9b7bf94c445667b4d552d9d7b1d6199df99c
nomhoi <n_vova@mail.ru>
2021-10-13 12:54:54 +0800

    Добавлена модель вопросов Question. Добавлена модель выборов на вопросы Choice. Добавлена модель ответов на текстовые вопросы TextResponse. В админку добавлена возможность добавления опросов, вопросов, вариантов ответов, текстовых ответов на вопросы.

--- project/polls/admin.py
+++ project/polls/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
-from .models import Pool
+from .models import Choice, Pool, TextResponse, Question
 
-admin.site.register(Pool)
+
+class QuestionInline(admin.StackedInline):
+    """Вопросы"""
+    model = Question
+    extra = 0
+
+
+@admin.register(Pool)
+class PoolAdmin(admin.ModelAdmin):
+    """Опросы"""
+    inlines = [QuestionInline]
+
+
+class ChoiceInline(admin.StackedInline):
+    """Выборы"""
+    model = Choice
+    extra = 0
+
+
+class TextResponseInline(admin.StackedInline):
+    """Текстовый ответы"""
+    model = TextResponse
+    extra = 0
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """Вопросы"""
+    inlines = [ChoiceInline, TextResponseInline]
--- project/polls/models.py
+++ project/polls/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 class Pool(models.Model):
     name = models.CharField('название', max_length=200)
     start_date = models.DateTimeField('дата начала')
     end_date = models.DateTimeField('дата окончания')
 
-    description = models.TextField('описание')
+    description = models.CharField('описание', max_length=200)
 
     class Meta:
         verbose_name = 'опрос'
@@ -13,3 +14,47 @@ class Pool(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Question(models.Model):
+    TYPES = (
+        (TEXT := 1, 'Ответ текстом'),
+        (ONE  := 2, 'Ответ с выбором одного варианта'),
+        (MANY := 3, 'Ответ с выбором нескольких вариантов'),
+    )
+
+    pool = models.ForeignKey(Pool, on_delete=models.CASCADE)
+    text = models.CharField('вопрос', max_length=200)
+    type = models.PositiveSmallIntegerField('Тип ответа', choices=TYPES)
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField('вариант ответа', max_length=200)
+    votes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'варианты ответов'
+        verbose_name_plural = 'варианты ответов'
+
+    def __str__(self):
+        return self.text
+
+
+class TextResponse(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField('ответ', max_length=200)
+
+    class Meta:
+        verbose_name = 'текстовые ответы'
+        verbose_name_plural = 'текстовые ответы'
+
+    def __str__(self):
+        return self.text
