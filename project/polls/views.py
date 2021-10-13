9a1103cb4f4c03ab36c487065174d7b7ca1c06a4
nomhoi <n_vova@mail.ru>
2021-10-13 16:35:23 +0800

    Получение списка опросов доступно для всех пользователей. Ограничен список разрешений для администратора: доступно только создание, удаление и обновление опросов.

--- project/polls/views.py
+++ project/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.exceptions import MethodNotAllowed, PermissionDenied
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from .models import Pool
 from .serializers import PoolSerializer
@@ -11,5 +12,17 @@ class PoolViewSet(viewsets.ModelViewSet):
     """
     queryset = Pool.objects.all()
     serializer_class = PoolSerializer
-    permission_classes = [IsAdminUser, IsAuthenticated]
-    
+    permission_classes_by_action = {
+        'create':   [IsAdminUser],
+        'destroy':  [IsAdminUser],
+        'list':     [IsAuthenticatedOrReadOnly],
+        'update':   [IsAdminUser],
+    }
+
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            raise MethodNotAllowed(self.action)
+            
