1d127a7adfad049ba6e6c4ba1931f8081a6f9ca5
nomhoi <n_vova@mail.ru>
2021-10-13 16:58:26 +0800

    Добавлен урл для авторизации. Исправлен урл для апи.

--- project/project/urls.py
+++ project/project/urls.py
@@ -10,5 +10,6 @@ router.register(r'polls', views.PoolViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/', include(router.urls)),
 ]
